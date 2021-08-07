d686a547926afcbebf5e8d6969a870bb7bb0f461
n4n0 <benzothedev@gmail.com>
2021-08-07 12:29:17 -0400

    Update wocky_termuix.py

--- wocky_termuix.py
+++ wocky_termuix.py
@@ -13,13 +13,13 @@ class WockyTermUIX:
     ## method -> img2txt(url)
     ## description -> converting image to asni text and returns the ASNI text URL
     def img2txt(url):
-        return requests.get(f"https://wocky.pw/img2txt/?key={apikey}&action=img2txt&url={url}").text
+        return requests.get(f"https://wocky.pw/termuix/?key={apikey}&action=img2txt&url={url}").text
 
     ## method -> Save_ASNI_To_File(filepath, url)
     ## argument note -> enter full filepath to file!
     ## description -> converting image to asni text then saving it to a file
     def GetASNICode(file_path, url):
-        asni_text = requests.get(f"https://wocky.pw/img2txt/?key={apikey}&action=img2txt&url={url}").text
+        asni_text = requests.get(f"https://wocky.pw/termuix/?key={apikey}&action=img2txt&url={url}").text
         try:
             asni_file = open(file_path, "w")
             asni_file.write(asni_text)
@@ -27,3 +27,8 @@ class WockyTermUIX:
             return f"ASNI Text saved to {file_path}"
         except:
             return "[x] Error, Unable to open file!"
+    
+    ## method -> gradient_fade(color1, color2, text)
+    ## description -> converting text into gradient color fade!
+    def gradient_fade(color1, color2, text):
+        return requests.get(f"https://wocky.pw/termuix/?key={apikey}&action=gradient-fade-save&text={text}&color1={color1}&color2={color2}").text
